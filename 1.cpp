8e855ca243a869d8ac021be4461d29e73edcb0ba
Vlad <khober.vflad@gmail.com>
2024-10-26 11:13:41 +0200

    Code C++

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+cout << '1' << '2' << '3' << endl;
+cout << setw(15) << showpos << right << 4 << endl;
+cout << setw(20) << internal << showpos << -67.09 << endl;
+cout << setw(15) << right << showpos << 235 << endl;
+cout << '8' << 'a' << '1' << endl;
+cout << fixed << setprecision(2) << showpos << -121.0 << endl;
+cout << setprecision(0) << showpos << 1 << 24 << endl;
+cout << "String1\n" << "\tString2 \n" << " \t\t String3" << endl;
+cout << "\tString1\n" << "String2" << endl;	
+cout << noshowpos <<3 << '*' << "\tString\n1" << endl;
