e780b8091169a79ce77fbb9568008eecb84a9661
Fechut <khober.vflad@gmail.co>
2024-11-02 21:13:42 +0200

    del 3.cpp

--- 3.cpp
+++
@@ -1,5 +0,0 @@
-double x, y, z, a;
-		cout << "Enter x, y, z\n";
-		cin >> x >> y >> z;
-		a = ((3 * abs(y) - pow(pow(x,8) + 4*y, 1/2. )) / (10 * x - y / pow(z, 6)) + pow(x, 5) / 4);
-		cout << "a = " << fixed << setprecision(2) << a << endl;
