d549ee32789dd9184a9dcf1f5350d21d3ed064fe
Fechut <khober.vflad@gmail.co>
2024-11-27 09:22:06 +0200

    add new cout

--- 1.cpp
+++ 1.cpp
@@ -7,3 +7,8 @@ cout << '8' << 'a' << '1' << endl;
 cout << setw(20) << internal << showpos << -67.09 << endl;
 cout << setw(15) << right << showpos << 235 << endl;
 cout << '8' << 'a' << '1' << endl;
+cout << '1' << '2' << '3' << endl;
+cout << setw(15) << showpos << right << 4 << endl;
+cout << setw(20) << internal << showpos << -67.09 << endl;
+cout << setw(15) << right << showpos << 235 << endl;
+cout << '8' << 'a' << '1' << endl;
